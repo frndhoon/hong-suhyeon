0bbe446b10b64ec3ad0278d6eabe6c726c6d0a00
frndhoon <frndhoon@gmail.com>
2024-12-15 22:11:29 +0900

    style: font 변경 감탄로드 바탕체

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
-import type { Metadata } from "next";
-import { Song_Myung } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
 
-const songMyung = Song_Myung({
-  variable: "--font-song-myung",
-  subsets: ["latin"],
-  weight: "400",
-});
-
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "김홍 ♥ 김수현 결혼합니다",
-  description: '2월 23일 오후 1시'
+  title: '김홍 ♥ 김수현 결혼합니다',
+  description: '2월 23일 오후 1시',
 };
 
 export default function RootLayout({
@@ -21,11 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body
-        className={`${songMyung.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
