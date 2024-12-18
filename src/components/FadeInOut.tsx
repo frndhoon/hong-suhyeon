1e273e4f4973bf47be622891737b972b8a37cfc6
frndhoon <frndhoon@gmail.com>
2024-12-18 17:43:48 +0900

    refactor: FadeInOut 애니메이션 classname 제거

--- src/components/FadeInOut.tsx
+++ src/components/FadeInOut.tsx
@@ -2,15 +2,9 @@
 
 import { motion } from 'framer-motion';
 
-export const FadeInOut = ({
-  children,
-}: {
-  className?: string;
-  children: React.ReactNode;
-}) => {
+export const FadeInOut = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      className="w-full flex flex-col items-center"
       initial={{ opacity: 0, y: 15 }}
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 1, ease: 'easeInOut', y: { duration: 1 } }}
