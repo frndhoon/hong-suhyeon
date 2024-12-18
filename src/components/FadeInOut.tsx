d32eab7b7678dd50bf6f051e7d306131a61b7d8e
frndhoon <frndhoon@gmail.com>
2024-12-18 17:54:46 +0900

    feat: FadeInOut Animation delay 및 viewport 한번만 보이게끔 수정

--- src/components/FadeInOut.tsx
+++ src/components/FadeInOut.tsx
@@ -2,13 +2,24 @@
 
 import { motion } from 'framer-motion';
 
-export const FadeInOut = ({ children }: { children: React.ReactNode }) => {
+export const FadeInOut = ({
+  children,
+  delay = 0,
+}: {
+  children: React.ReactNode;
+  delay?: number;
+}) => {
   return (
     <motion.div
       initial={{ opacity: 0, y: 15 }}
       whileInView={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1, ease: 'easeInOut', y: { duration: 1 } }}
-      viewport={{ once: false }}
+      transition={{
+        duration: 1,
+        ease: 'easeInOut',
+        y: { duration: 1 },
+        delay: delay,
+      }}
+      viewport={{ once: true }}
     >
       {children}
     </motion.div>
